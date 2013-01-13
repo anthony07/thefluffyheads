8aeaec44c309752f6a83bd15da260c8689cc2eda
Anthony Eliezer Arellano <anthony.e.arellano@gmail.com>
2013-01-13 15:50:23 +0800

    sidebar and footer changes

--- js/document_ready.js
+++ js/document_ready.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function() {
-  var $ = jQuery, doc = $(this), shadow = $('#header-shadow'), main = $('#main');
+  var $ = jQuery, doc = $(this), shadow = $('#header-shadow'), main = $('#main'),
+    wind0w = $(window), footer = $("#footer");
 
   $('.white-paper .node:even').addClass('even');
   $('.white-paper .node:odd').addClass('odd');
@@ -7,6 +8,19 @@ jQuery(document).ready(function() {
   $('img').removeAttr('width').removeAttr('height');
   //This for the Pager to have room for margin
   $('article.node:last-of-type').addClass('clearfix');
+
+  //Adjust footer contents in load and resize
+  var adjustFooter = function() {
+    var width = wind0w.width(), footerBase = $('#footer-base');
+    if(width < 1024) {
+      footer.css("left", 0);
+    } else {
+      var footerLeft = (width-1024)/2;
+      footer.css("left", footerLeft);
+    }
+  }
+  adjustFooter();
+  wind0w.resize(adjustFooter);
 
   //Show shadow under header when #main is under
   doc.scroll(function() {
--- page.tpl.php
+++ page.tpl.php
@@ -1,105 +1,111 @@
 <div id="container" class="clearfix">
-
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-    <?php if ($main_menu): ?>
-      <a href="#navigation" class="element-invisible element-focusable"><?php print t('Skip to navigation'); ?></a>
-    <?php endif; ?>
-  </div>
-
-  <header id="header" role="banner" class="clearfix">
-    <div class="wrapper">
-  	<?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" id="logo">
-          <img src="http://files.thefluffyheads.com/logoness.png" alt="Home" rel="logo" />
-        </a>
+  <div id="wrapper" class="clearfix">
+    <div id="skip-link">
+      <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+      <?php if ($main_menu): ?>
+        <a href="#navigation" class="element-invisible element-focusable"><?php print t('Skip to navigation'); ?></a>
       <?php endif; ?>
-      <?php if ($site_name || $site_slogan): ?>
-        <hgroup id="site-name-slogan">
-          <?php if ($site_name): ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-          <?php if ($site_slogan): ?>
-            <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-          <?php endif; ?>
-        </hgroup>
-      <?php endif; ?>
-
-      <?php print render($page['header']); ?>
     </div>
-  </header> <!-- /#header -->
 
-  <div id="header_gutter" class="empty"></div>
-  <div id="header-shadow" class="empty block"></div>
+    <header id="header" role="banner" class="clearfix">
+      <div class="wrapper">
+    	<?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" id="logo">
+            <img src="http://files.thefluffyheads.com/logoness.png" alt="Home" rel="logo" />
+          </a>
+        <?php endif; ?>
+        <?php if ($site_name || $site_slogan): ?>
+          <hgroup id="site-name-slogan">
+            <?php if ($site_name): ?>
+              <h1 id="site-name">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><span><?php print $site_name; ?></span></a>
+              </h1>
+            <?php endif; ?>
+            <?php if ($site_slogan): ?>
+              <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
+            <?php endif; ?>
+          </hgroup>
+        <?php endif; ?>
 
-  <?php if ($main_menu || $secondary_menu || !empty($page['navigation'])): ?>
-  <nav id="navigation" role="navigation" class="clearfix">
-    <?php if (!empty($page['navigation'])): ?> <!--if block in navigation region, override $main_menu and $secondary_menu-->
-      <?php print render($page['navigation']); ?>
-    <?php endif; ?>
-    <?php if (empty($page['navigation'])): ?>
-    <?php print theme('links__system_main_menu', array(
-        'links' => $main_menu,
-        'attributes' => array(
-          'id' => 'main-menu',
-          'class' => array('links', 'clearfix'),
-        ),
-        'heading' => array(
-          'text' => t('Main menu'),
-          'level' => 'h2',
-          'class' => array('element-invisible'),
+        <?php print render($page['header']); ?>
+      </div>
+    </header> <!-- /#header -->
+
+    <div id="header-shadow" class="empty block"></div>
+
+    <?php if ($main_menu || $secondary_menu || !empty($page['navigation'])): ?>
+    <nav id="navigation" role="navigation" class="clearfix">
+      <?php if (!empty($page['navigation'])): ?> <!--if block in navigation region, override $main_menu and $secondary_menu-->
+        <?php print render($page['navigation']); ?>
+      <?php endif; ?>
+      <?php if (empty($page['navigation'])): ?>
+      <?php print theme('links__system_main_menu', array(
+          'links' => $main_menu,
+          'attributes' => array(
+            'id' => 'main-menu',
+            'class' => array('links', 'clearfix'),
           ),
-        )); ?>
-    <?php print theme('links__system_secondary_menu', array(
-            'links' => $secondary_menu,
-            'attributes' => array(
-              'id' => 'secondary-menu',
-              'class' => array('links', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Secondary menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
+          'heading' => array(
+            'text' => t('Main menu'),
+            'level' => 'h2',
+            'class' => array('element-invisible'),
             ),
           )); ?>
-        <?php endif; ?>
-    <div id="nav-spanner">End of navigation</div>
-  </nav> <!-- /#navigation -->
-  <?php endif; ?>
-  <section id="main" role="main" class="<?php ($is_front) ? print 'front-page' : print 'article-page'; ?>">
-    <?php print $messages; ?>
-  <?php if ($page['highlighted']): ?>
-    <div id="highlighted" class="clearfix"><?php print render($page['highlighted']); ?></div>
-  <?php endif; ?>
-    <div class="white-paper">
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper"><?php print render($tabs); ?></div><?php endif; ?>
-      <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-      <?php print render($page['content']); ?>
-    </div>
-  </section> <!-- /#main -->
-
-  <section id="sidebars" role="complementary" class="clearfix">
-    <?php if ($page['sidebar_first']): ?>
-      <aside id="sidebar-first" role="complementary" class="sidebar">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
+      <?php print theme('links__system_secondary_menu', array(
+              'links' => $secondary_menu,
+              'attributes' => array(
+                'id' => 'secondary-menu',
+                'class' => array('links', 'clearfix'),
+              ),
+              'heading' => array(
+                'text' => t('Secondary menu'),
+                'level' => 'h2',
+                'class' => array('element-invisible'),
+              ),
+            )); ?>
+          <?php endif; ?>
+      <div id="nav-spanner">End of navigation</div>
+    </nav> <!-- /#navigation -->
     <?php endif; ?>
-
-    <?php if ($page['sidebar_second']): ?>
-      <aside id="sidebar-second" role="complementary" class="sidebar">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
+    <section id="main" role="main" class="<?php ($is_front) ? print 'front-page' : print 'article-page'; ?>">
+      <?php print $messages; ?>
+    <?php if ($page['highlighted']): ?>
+      <div id="highlighted" class="clearfix"><?php print render($page['highlighted']); ?></div>
     <?php endif; ?>
-  </section>
+      <div class="white-paper">
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php if (!empty($tabs['#primary'])): ?><div class="tabs-wrapper"><?php print render($tabs); ?></div><?php endif; ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        <?php print render($page['content']); ?>
+      </div>
+    </section> <!-- /#main -->
 
-  <footer id="footer" role="contentinfo" class="clearfix">
-    <?php print render($page['footer']) ?>
-  </footer> <!-- /#footer -->
+    <section id="sidebars" role="complementary" class="clearfix">
+      <?php if ($page['sidebar_first']): ?>
+        <aside id="sidebar-first" role="complementary" class="sidebar">
+          <?php print render($page['sidebar_first']); ?>
+        </aside>  <!-- /#sidebar-first -->
+      <?php endif; ?>
 
+      <?php if ($page['sidebar_second']): ?>
+        <aside id="sidebar-second" role="complementary" class="sidebar">
+          <?php print render($page['sidebar_second']); ?>
+        </aside>  <!-- /#sidebar-second -->
+      <?php endif; ?>
+    </section>
+  </div>
 </div> <!-- /#container -->
+
+<footer id="footer" role="contentinfo" class="clearfix">
+  <?php print render($page['footer']) ?>
+  <div id="footer-art">
+    <a class="about-us" title="Abous Us" href="/about-us">About Us</a>
+    <a class="site-map" title="Site Map" href="/site-map">Site Map</a>
+    <a class="back-to-top" title="Back to Top" href="#">Back to Top</a>
+    <img id="footer-base" src="http://files.thefluffyheads.com/assets/sprites/base.png" alt="footer-base.png" />
+  </div>
+</footer> <!-- /#footer -->
+
 <?php if(isset($story_proper)) print 'hello'; ?>
